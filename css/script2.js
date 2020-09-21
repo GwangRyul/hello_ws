771c07711766e031691f33b4e626501c2b7c0250
GwangRyul <tk34521@naver.com>
2020-09-21 16:37:07 +0900

    PROJECT

--- css/script2.js
+++ css/script2.js
@@ -1,6 +1,11 @@
 window.onload = function(){
-    var hw = document.getElementById('hw');
+    /*var hw = document.getElementById('hw');
     hw.addEventListener('click', function(){
         alert('Hello world');
-    })
+    })*/
+    var i = 0;
+    while(i < 10){
+        document.write('coding everybody '+i+'<br />');
+        i++;
+    }
 }
